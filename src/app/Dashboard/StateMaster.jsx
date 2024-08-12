6f75abb86881d9f99bed36eb379969d3fabcb848
ujjavalsrivastava <60099892+ujjavalsrivastava@users.noreply.github.com>
2024-08-12 23:35:19 +0530

    chnge

--- src/app/Dashboard/StateMaster.jsx
+++ src/app/Dashboard/StateMaster.jsx
@@ -5,7 +5,15 @@ import { fetchData } from "../../helper/api";
 const StateMaster = () => {
 
    
-    const[data,setData] = useState(null);
+    const[data,setData] = useState([]);
+
+    const[save,setsave] = useState({
+           
+      "stateName": null,
+      "districtName": null,
+      "cityName": null,
+      "pinCode": null,
+  });
 
     const fetchState = async()=>{
        try { 
@@ -20,6 +28,39 @@ const StateMaster = () => {
            toast.error(error.message);
          }
     }
+
+ const handleChange = (e)=>{
+  setsave((pre)=>(
+    {
+      ...pre,
+      [e.target.name]:e.target.value
+    }
+  ))
+ }
+
+ const savestateFun = async(e)=>{
+        e.preventDefault();
+        try {
+    
+   
+          const response = await fetchData('/user/saveNewState', {
+            method: 'POST',
+            body: JSON.stringify(save),
+        });
+         
+          if (response.status) {
+          
+            toast.success(response.msg);
+            fetchState();
+          } else {
+            toast.error(response.msg);
+          }
+        } catch (error) {
+          toast.error(error.message);
+        }
+ }
+
+
     useEffect(()=>{
         fetchState();
     },[])
@@ -29,31 +70,50 @@ const StateMaster = () => {
   <div class="content-wrapper"> 
     
     <div class="content-header sty-one">
-      <h1 class="text-black">Basic Tables</h1>
+      <h1 class="text-black">State master</h1>
       <ol class="breadcrumb">
         <li><a href="#">Home</a></li>
-        <li class="sub-bread"><i class="fa fa-angle-right"></i> Tables</li>
-        <li><i class="fa fa-angle-right"></i> Basic Tables</li>
+        <li class="sub-bread"><i class="fa fa-angle-right"></i> Form</li>
+        <li><i class="fa fa-angle-right"></i> State</li>
       </ol>
     </div>
     
     
     <div class="content">
       <div class="info-box">
+        <form onSubmit={savestateFun}>
         <div class="row">
           <div class="col-md-6">
-            <div class="form-group has-feedback">
-              <label class="control-label">Select State</label>
-              <select class="custom-select form-control" id="location1" name="location">
-                <option value="">Select State</option>
-                <option value="Amsterdam">Example</option>
-                <option value="Berlin">Example</option>
-                <option value="Frankfurt">Example</option>
-              </select>
-             
+           <div class="form-group has-feedback">
+                    <label class="control-label">State Name</label>
+                    <input class="form-control" name="stateName" type="text" onChange={(e)=>handleChange(e)} />
+                   
+            </div>
+          </div>
+
+          <div class="col-md-6">
+           <div class="form-group has-feedback">
+                    <label class="control-label">District Name</label>
+                    <input class="form-control" name="districtName" type="text" onChange={(e)=>handleChange(e)} />
+                   
+            </div>
+          </div>
+
+          <div class="col-md-6">
+           <div class="form-group has-feedback">
+                    <label class="control-label">City</label>
+                    <input class="form-control" name="cityName" type="text" onChange={(e)=>handleChange(e)} />
+                   
             </div>
           </div>
           <div class="col-md-6">
+           <div class="form-group has-feedback">
+                    <label class="control-label">Pin Code</label>
+                    <input class="form-control" name="pinCode" type="text" onChange={(e)=>handleChange(e)} />
+                   
+            </div>
+          </div>
+          {/* <div class="col-md-6">
             <div class="form-group has-feedback">
               <label class="control-label">Select District</label>
               <select class="custom-select form-control" id="location1" name="location">
@@ -76,13 +136,14 @@ const StateMaster = () => {
               </select>
             
             </div>
-          </div>
+          </div> */}
           <div class="col-md-6">
             <div class="form-group has-feedback">
               <button type="submit" class="btn btn-success" style={{marginTop: '29px'}}>Submit</button>
             </div>
           </div>
           </div>
+          </form>
         <div class="table-responsive">
           <table class="table">
             <thead>
